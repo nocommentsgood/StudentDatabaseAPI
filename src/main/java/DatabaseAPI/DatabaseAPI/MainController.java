e0222f08b531c06e7a46b912eb94166e379c73f3
nocommentsgood <boxspring18@gmail.com>
2022-08-17 18:01:01 -0500

    Add local Cross-Origin Resource Sharing.

--- src/main/java/DatabaseAPI/DatabaseAPI/MainController.java
+++ src/main/java/DatabaseAPI/DatabaseAPI/MainController.java
@@ -3,14 +3,16 @@ package DatabaseAPI.DatabaseAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
 
 @Controller
-@RequestMapping(path="/demo")
+@RequestMapping(path="/api")
 public class MainController {
     @Autowired
     private StudentRepository studentRepository;
 
-    @PostMapping(path="/add")
+    @CrossOrigin
+    @PostMapping(path="/student")
     public @ResponseBody String addNewStudent (@RequestParam(value="firstname") String firstname, @RequestParam(value="lastname") String lastname,
                                                @RequestParam(value="studentyear") Integer studentyear, @RequestParam(value="balance") Integer balance){
         Student student = new Student();
@@ -22,9 +24,16 @@ public class MainController {
         return "Saved";
     }
 
-    @GetMapping(path="/all")
+    @CrossOrigin
+    @GetMapping(path="/students")
     public @ResponseBody Iterable<Student> getAllStudents(){
         // this returns a JSON or XML with the students
         return studentRepository.findAll();
+    }
+
+    @CrossOrigin
+    @GetMapping(path="/id")
+    public Optional<Student> getStudent(@RequestParam(value="studentid") Integer studentid) {
+        return studentRepository.findById(studentid);
     }
 }
